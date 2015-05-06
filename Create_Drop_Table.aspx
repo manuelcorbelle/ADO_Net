30e169c698c368e2e38418acb0eaddaa7b6c0d21
manuelcorbelle <manuelcorbelle@yahoo.es>
2015-05-06 20:11:24 +0200

    Crear y Borrar Tablas desde ADO.Net

---
+++ Create_Drop_Table.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Create_Drop_Table.aspx.vb" Inherits="BD_Operaciones.Create_Drop_Table" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Button ID="btnCreateTable" Text="Create Table" runat="server" OnClick="CreateTable" />
+    <asp:Button ID="btnDropTable" Text="Drop Table" runat="server" OnClick="DropTable" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Create_Drop_Table.aspx.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Data
+Imports System.Configuration
+Imports System.Data.SqlClient
+
+Public Class Create_Drop_Table
+    Inherits System.Web.UI.Page
+
+    Protected Sub CreateTable(sender As Object, e As EventArgs)
+        Dim query As String = "IF OBJECT_ID('dbo.CustomersTest', 'U') IS NULL "
+        query += "BEGIN "
+        query += "CREATE TABLE [dbo].[CustomersTest]("
+        query += "[CustomerId] INT IDENTITY(1,1) NOT NULL CONSTRAINT pkCustomerId PRIMARY KEY,"
+        query += "[Name] VARCHAR(100) NOT NULL,"
+        query += "[Country] VARCHAR(50) NOT NULL"
+        query += ")"
+        query += " END"
+        Label2.Visible = False
+        Label1.Visible = True
+        Label1.Text = "Tabla Creada......."
+        Dim constr As String = ConfigurationManager.ConnectionStrings("constr").ConnectionString
+        Using con As New SqlConnection(constr)
+            Using cmd As New SqlCommand(query)
+                cmd.Connection = con
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+            End Using
+        End Using
+    End Sub
+
+    Protected Sub DropTable(sender As Object, e As EventArgs)
+        Dim query As String = "IF OBJECT_ID('dbo.CustomersTest', 'U') IS NOT NULL "
+        query += "BEGIN "
+        query += "DROP TABLE dbo.CustomersTest "
+        query += "END"
+        Label1.Visible = False
+        Label1.Text = "Tabla Creada......."
+        Label2.Visible = True
+        Label2.Text = "Tabla Borrada......."
+        Dim constr As String = ConfigurationManager.ConnectionStrings("constr").ConnectionString
+        Using con As New SqlConnection(constr)
+            Using cmd As New SqlCommand(query)
+                cmd.Connection = con
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+            End Using
+        End Using
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Label1.Visible = False
+        Label2.Visible = False
+    End Sub
+End Class
